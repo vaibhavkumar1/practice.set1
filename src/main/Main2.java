07d0fdf787cacee7a73178164f296ea82652b1d9
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-22 23:57:16 +0530

    read a char

---
+++ src/main/Main2.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char a = s.next().charAt(0);
+
+    }
+}
