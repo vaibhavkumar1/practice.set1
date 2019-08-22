f1948860a5658cf064ccc2fe28a4cb655197da9e
vaibhav kumar <vaibhav.kumar_cs18@gla.ac.in>
2019-08-23 00:12:30 +0530

    find input char are digit,alphabet,special character

--- src/main/Main2.java
+++ src/main/Main2.java
@@ -6,6 +6,10 @@ public class Main2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         char a = s.next().charAt(0);
-
+        if ((int) a >= 65 && (int) a <= 122)
+            System.out.println("Alphabet");
+        else if ((int) a > 127 || (int) a < 65)
+            System.out.println("Special character");
+        else System.out.println("Digit");
     }
 }
